879945d2075fa3e5c347307d03f22a201cd5028d
Mariah Virnig <mvirnig1@css.edu>
2019-11-01 14:38:06 -0500

    Request  407

--- SingleResponsibilityPrinciple/StreamTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/StreamTradeDataProvider.cs
@@ -27,5 +27,21 @@ namespace SingleResponsibilityPrinciple
         }
 
         private readonly Stream stream;
+    
+    public IEnumerable<string> URLGetTradeData()
+        {
+           var tradeData = new List (); 
+            var client = new WebClient(); 
+            using (var stream = client.OpenRead(url)) 
+            using (var reader = new StreamReader(stream)) 
+            { 
+                string line; 
+                while ((line = reader.ReadLine()) != null) 
+                { 
+                    tradeData.Add(line); 
+                } 
+            } 
+            return tradeData;
+        }
     }
 }
